b3628c6850e93723b1351596291125f9790a10f0
tswowo6 <tswossion@outlook.com>
2025-12-02 11:35:51 +0800

    IO流

---
+++ 2.JavaSe2/src/IOFile/CopyFile1.java
@@ -0,0 +1,19 @@
+package IOFile;
+
+import java.io.*;
+import java.util.Collections;
+
+public class CopyFile1 {
+    public static void main(String[] args) {
+        String path = "2.JavaSe2/src/IOFile/24.jpg";
+        byte[] bs = new byte[(1 << 20)];
+        try (InputStream is = new FileInputStream(path);
+             OutputStream os = new FileOutputStream("2.JavaSe2/src/IOFile/24_copy.jpg");
+        ) {
+            int len = is.read(bs);
+            os.write(bs, 0, len);
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+}
